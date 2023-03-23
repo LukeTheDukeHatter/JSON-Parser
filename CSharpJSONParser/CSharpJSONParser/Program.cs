41c9b146ff668b18c809a4531915ae2c87e295c2
Luke Harrison <luketheduke.harrison@gmail.com>
2023-03-23 23:29:07 +0000

    More C#

--- CSharpJSONParser/CSharpJSONParser/Program.cs
+++ CSharpJSONParser/CSharpJSONParser/Program.cs
@@ -1,7 +1,10 @@
-﻿Dictionary<string, object> SET_VALUES = new Dictionary<string, object>(){ {"false", false}, {"true", true}, {"null", null} };
+﻿using System.Text.RegularExpressions;
+
+Dictionary<string, object> SET_VALUES = new Dictionary<string, object>(){ {"false", false}, {"true", true}, {"null", null} };
 char[] VALID_WHITESPACE = { ' ', ' ', ' ', ' ' };
 string VALID_CHARS = "[]{}\":,trufasnl+-e.0123456789";
 string[] POSSIBLE_MISSED = { ",:", ":,", ",,", "[]", "[,", ",]", ":}", "{:" };
+Regex NUMBER_REGEX = new Regex(@"^-?[0-9]+(\.[0-9]+)?((e|E)(\+|-)?[0-9]+)?$");
 
 string clean_whitespace(string data)
 {
@@ -27,9 +30,9 @@ string clean_whitespace(string data)
     return output;
 }
 
-int get_closing_index(data, start_index) {
+int get_closing_index(string data,int start_index) {
     char open_type = data[start_index];
-    Dictionary<char,char> = new Dictionary<char,char>() {{'[',']'}, {'{','}'}, {'"','"'} };
+    Dictionary<char,char> opposite = new Dictionary<char,char>() {{'[',']'}, {'{','}'}, {'"','"'} };
     bool in_string = false;
     int bracket_count = 0;
     for (int i = start_index; i<data.Length; i++) {
@@ -41,31 +44,107 @@ int get_closing_index(data, start_index) {
             in_string = !in_string;
         }
 
-        if (bracket_count) == 0 {
+        if (bracket_count == 0) {
             return i;
         }
     }
+    throw new JSONError("Invalid JSON: Unmatched brackets or quotes");
 }
 
-List<KeyValuePair<char,int>> get_all_markers(data) {
+List<KeyValuePair<char,int>> get_all_markers(List<object> data) {
     string new_markers = "[]{}\",:";
     bool in_string = false;
     List<KeyValuePair<char,int>> found_markers = new List<KeyValuePair<char,int>>();
 
-    for (int i = 0; i < data.Length; i++) {
-        char key = data[i];
-        if (key == '"' && data[i > 0 ? i-1 : i] != '\\') {
+    for (int i = 0; i < data.Count; i++) {
+        char key = (char)data[i];
+        if (key == '"' && (char)data[i > 0 ? i-1 : i] != '\\') {
             in_string = !in_string;
-            found_markers.Append(new KeyValuePair<char,int>() {key, i});
+            found_markers.Append(new KeyValuePair<char,int>(key, i));
         }
         if (new_markers.Contains(key) && !in_string && key != '"') {
-            found_markers.Append(new KeyValuePair<char,int>() {key, i});
-        }
-
-        Dictionary<char,int> mc = new Dictionary<char,int>() { {'{', 0}, {'[', 0}, {'"', 0} };
-
-        foreach (KeyValuePair<char,int> in found_markers) {
-            
+            found_markers.Append(new KeyValuePair<char,int>(key, i));
         }
     }
+
+    Dictionary<char,int> mc = new Dictionary<char,int>() { {'{', 0}, {'[', 0}, {'"', 0} };
+
+    foreach (KeyValuePair<char,int> i in found_markers) {
+        if (i.Key == '{' || i.Key == '}') {
+            mc['{'] += 1;
+        } else if (i.Key == '[' || i.Key == ']')
+        {
+            mc['['] += 1;
+        } else if (i.Key == '"')
+        {
+            mc['"'] += 1;
+        }
+    }
+
+    if ((mc['{'] % 2 != 0) || (mc['['] % 2 != 0) || (mc['"'] % 2 != 0)) { throw new JSONError("Invalid JSON: Unmatched brackets or quotes"); }
+
+    return found_markers;
 }
+
+Token ValueValidator(string value)
+{
+    if (SET_VALUES.ContainsKey(value))
+    {
+        return new Token(value, SET_VALUES[value]);
+    }
+    else if (NUMBER_REGEX.IsMatch(value))
+    {
+        return new Token("number", float.Parse(value));
+    }
+    return null;
+}
+
+Dictionary<string, object> parse_object(string data_big, bool r = false)
+{
+    List<object> list_data_big = new List<object>() { };
+    foreach (char c in data_big) { list_data_big.Add(c); }
+
+    if (!r)
+    {
+        List<KeyValuePair<char, int>> all_markers = get_all_markers(list_data_big);
+        List<Dictionary<string, object>> missed_values = new List<Dictionary<string, object>>();
+
+        for (int m_index = 0; m_index < all_markers.Count - 1; m_index++)
+        {
+            if (POSSIBLE_MISSED.Contains(all_markers[m_index].Key.ToString() + all_markers[m_index+1].Key.ToString()))
+            {
+                int start_index = all_markers[m_index].Value;
+                int end_index = all_markers[m_index+1].Value;
+
+                // Token missed = ValueValidator('a');
+            }
+        }
+
+    }
+
+
+    return new Dictionary<string, object>() { };
+}
+
+
+
+class Token
+{
+    public string Type { get; }
+    public object Value { get; }
+
+    public Token(string tokenType, object value = null)
+    {
+        Type = tokenType;
+        Value = value;
+    }
+}
+
+class JSONError : Exception
+{
+    public JSONError(string message) : base(message)
+    {
+    }
+}
+
+
